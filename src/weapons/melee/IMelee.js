43c08d3107230cbfba23558e0278de41f752d4a3
MicMetz <mime9599@colorado.edu>
2023-01-23 01:39:07 -0700

    weapon interface

--- src/weapons/melee/IMelee.js
+++ src/weapons/melee/IMelee.js
@@ -1,4 +1,5 @@
-import {Weapon} from "../Weapon.js";
+import {Raycaster} from "three";
+import {Weapon}    from "../Weapon.js";
 
 
 
@@ -9,10 +10,43 @@ class IMelee extends Weapon {
     * @param mesh
     */
    constructor(owner, mesh) {
-      super(owner, mesh);
+      super(owner);
 
-      this._damage = 0;
-      this._range  = 0;
+      this.weapon      = mesh;
+      this.damage      = 1;
+      this.range       = 1;
+      this.raycaster   = new Raycaster(undefined, undefined, 0, undefined);
+      this.enemy       = null;
+      this.isAttacking = false;
+
+      this.engage    = () => {
+         this.isAttacking = true;
+         this.owner.stateMachine.setState("melee");
+      }
+      this.disengage = () => {
+         this.isAttacking = false;
+         this.owner.stateMachine.setState("idle");
+      }
+      this.attack    = () => {
+         if (this.isAttacking) {
+            this.raycaster.set(this.owner.position, this.owner.direction);
+            this.enemy = this.raycaster.intersectObjects(this.owner.world.enemies.children, true);
+            if (this.enemy.length > 0) {
+               this.enemy[0].object.parent.takeDamage(this);
+            }
+         }
+      }
+
 
    }
+
+
+   collide(hitting) {
+
+
+
+   }
+
+
+
 }
--- src/weapons/ranged/IRanged.js
+++ src/weapons/ranged/IRanged.js
@@ -1,4 +1,5 @@
-import {Weapon} from "../Weapon.js";
+import {Raycaster} from "three";
+import {Weapon}    from "../Weapon.js";
 
 
 
@@ -11,8 +12,41 @@ class IRanged extends Weapon {
    constructor(owner, mesh) {
       super(owner, mesh);
 
-      this._damage = 0;
-      this._range  = 0;
+      this.weapon    = mesh;
+      this.damage    = 1;
+      this.range     = 1;
+      this.raycaster = new Raycaster(undefined, undefined, 0, undefined);
+      this.enemy     = null;
+
+      this.engage    = () => {
+         this.isAttacking = true;
+         this.owner.stateMachine.setState("shoot");
+      }
+      this.disengage = () => {
+         this.isAttacking = false;
+         if (this.owner.stateMachine) {
+            if (this.owner.stateMachine.states.has("shootIdle")) {
+               this.owner.stateMachine.setState("shootIdle");
+            } else {
+               this.owner.stateMachine.setState("idle");
+            }
+         }
+      }
+      this.attack    = () => {
+         if (this.isAttacking) {
+            this.raycaster.set(this.owner.position, this.owner.direction);
+            this.enemy = this.raycaster.intersectObjects(this.owner.world.enemies.children, true);
+            if (this.enemy.length > 0) {
+               this.enemy[0].object.parent.takeDamage(this);
+            }
+         }
+      }
+   }
+
+
+   collide(hitting) {
 
    }
+
+
 }
